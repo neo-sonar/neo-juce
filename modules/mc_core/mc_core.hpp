d86b465c52ee17451a942913a393e134618b6976
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-03-06 22:31:14 +0100

    Add fmt submodule & create mc_core module

---
+++ modules/mc_core/mc_core.hpp
@@ -0,0 +1,38 @@
+/*
+ ==============================================================================
+ BEGIN_JUCE_MODULE_DECLARATION
+
+ ID:            mc_core
+ vendor:        Modern Circuits
+ version:       0.1.0
+ name:          Core
+ description:   Core JUCE extensions and utilities.
+ dependencies:  juce_gui_extra, juce_events, juce_dsp
+ website:       https://modern-circuits.com
+ license:       Copyright 2019-2021 Modern Circuits. All rights reserved.
+
+ END_JUCE_MODULE_DECLARATION
+
+ ==============================================================================
+
+ @defgroup mc_core
+
+ ==============================================================================
+ */
+
+#ifndef MODERN_CIRCUITS_JUCE_MODULES_MC_CORE_HPP
+#define MODERN_CIRCUITS_JUCE_MODULES_MC_CORE_HPP
+
+#ifndef USE_MC_CORE
+#define USE_MC_CORE 1
+#endif
+
+#include <juce_dsp/juce_dsp.h>
+#include <juce_events/juce_events.h>
+#include <juce_gui_extra/juce_gui_extra.h>
+
+#include "fmt/chrono.h"
+#include "fmt/format.h"
+#include "fmt/printf.h"
+
+#endif  // MODERN_CIRCUITS_JUCE_MODULES_MC_CORE_HPP
