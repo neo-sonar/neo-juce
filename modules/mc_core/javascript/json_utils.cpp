30293f9bbacd44912a303a5fe6c1e9f04ae4cba3
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-03-08 00:26:31 +0100

    [core] Add tests to JSONUtils::loadFromURL

--- modules/mc_core/javascript/json_utils.cpp
+++ modules/mc_core/javascript/json_utils.cpp
@@ -4,6 +4,7 @@ namespace mc
 auto JSONUtils::loadFromURL(juce::URL const& url) -> juce::var
 {
     auto response = url.createInputStream(false);
+    jassert(response != nullptr);
     return juce::JSON::parse(*response);
 }
 }  // namespace mc
---
+++ modules/mc_core/javascript/json_utils_test.cpp
@@ -0,0 +1,10 @@
+#include "mc_core/mc_core.hpp"
+
+#include "catch2/catch_test_macros.hpp"
+
+TEST_CASE("core/javascript: JSONUtils::loadFromURL", "[core][javascript]")
+{
+    auto url  = juce::URL {"https://jsonplaceholder.typicode.com/users"};
+    auto json = mc::JSONUtils::loadFromURL(url);
+    CHECK(json.getArray()->size() == 10);
+}
