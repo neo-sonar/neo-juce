ad33f63d6db78b559061afbda53fbb380c5c4b5f
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-03-08 01:14:01 +0100

    [modulation] Add tests to Modulator

--- modules/mc_modulation/modulator/modulator_test.cpp
+++ modules/mc_modulation/modulator/modulator_test.cpp
@@ -13,17 +13,30 @@ TEST_CASE("modulation/modulator: Modulator", "[modulation][modulator]")
 
     struct TestPlayHead : juce::AudioPlayHead
     {
-        TestPlayHead() : juce::AudioPlayHead {} { }
+        ~TestPlayHead() override = default;
         auto getCurrentPosition(juce::AudioPlayHead::CurrentPositionInfo&) -> bool override { return false; }
         auto canControlTransport() -> bool override { return false; }
     };
 
+    // defaults
     auto modulator = TestModulator {};
     CHECK(modulator.getValueTree().hasType("test"));
     CHECK(modulator.getName() == juce::String {""});
     CHECK(modulator.getUndoManager() == nullptr);
     CHECK(modulator.getPlayHead() == nullptr);
 
+    // default impl should not change the buffer
+    auto midiBuffer = juce::MidiBuffer {};
+    auto buffer     = juce::AudioBuffer<float> {1, 32};
+    buffer.clear();
+
+    modulator.prepareToPlay(44'100.0, 32);
+    modulator.processBlock(buffer, midiBuffer);
+    auto* const output = buffer.getWritePointer(0);
+    for (auto i = 0; i < buffer.getNumSamples(); ++i) { CHECK(output[i] == 0.0f); }
+    modulator.releaseResources();
+
+    // play head
     auto playHead = TestPlayHead {};
     modulator.setPlayHead(&playHead);
     CHECK(modulator.getPlayHead() == &playHead);
