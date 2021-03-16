94546d038d2690cd7d7b7e1425d8b2a76c765cb1
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-03-16 20:18:11 +0100

    [gui_basics] Cleanup attachments

---
+++ modules/mc_gui_basics/attachment/slider_attachment.cpp
@@ -0,0 +1,31 @@
+namespace mc
+{
+
+SliderValueTreeAttachment::SliderValueTreeAttachment(juce::ValueTree state, juce::Identifier const& id,
+                                                     juce::Slider& slider, juce::UndoManager* undoManager)
+    : slider_ {slider}, attachment_ {std::move(state), id, [this](auto f) { setValue(f); }, undoManager}
+{
+    sendInitialUpdate();
+    slider_.valueChanged();
+    slider_.addListener(this);
+}
+
+SliderValueTreeAttachment::~SliderValueTreeAttachment() { slider_.removeListener(this); }
+
+void SliderValueTreeAttachment::sendInitialUpdate() { attachment_.sendInitialUpdate(); }
+
+void SliderValueTreeAttachment::setValue(float newValue)
+{
+    juce::ScopedValueSetter<bool> svs(ignoreCallbacks_, true);
+    slider_.setValue(newValue, juce::sendNotificationSync);
+}
+void SliderValueTreeAttachment::sliderValueChanged(juce::Slider* /*slider*/)
+{
+    if (ignoreCallbacks_ || juce::ModifierKeys::currentModifiers.isRightButtonDown()) { return; }
+    attachment_.setValueAsPartOfGesture(static_cast<float>(slider_.getValue()));
+}
+
+void SliderValueTreeAttachment::sliderDragStarted(juce::Slider* /*slider*/) { attachment_.beginGesture(); }
+void SliderValueTreeAttachment::sliderDragEnded(juce::Slider* /*slider*/) { attachment_.endGesture(); }
+
+}  // namespace mc
--- modules/mc_gui_basics/attachment/slider_attachment.hpp
+++ modules/mc_gui_basics/attachment/slider_attachment.hpp
@@ -7,32 +7,16 @@ class SliderValueTreeAttachment : private juce::Slider::Listener
 {
 public:
     SliderValueTreeAttachment(juce::ValueTree state, juce::Identifier const& id, juce::Slider& slider,
-                              juce::UndoManager* undoManager = nullptr)
-        : slider_ {slider}, attachment_ {std::move(state), id, [this](auto f) { setValue(f); }, undoManager}
-    {
-        sendInitialUpdate();
-        slider_.valueChanged();
-        slider_.addListener(this);
-    }
-
-    ~SliderValueTreeAttachment() override { slider_.removeListener(this); }
-
-    void sendInitialUpdate() { attachment_.sendInitialUpdate(); }
+                              juce::UndoManager* undoManager = nullptr);
+    ~SliderValueTreeAttachment() override;
+    void sendInitialUpdate();
 
 private:
-    void setValue(float newValue)
-    {
-        juce::ScopedValueSetter<bool> svs(ignoreCallbacks_, true);
-        slider_.setValue(newValue, juce::sendNotificationSync);
-    }
-    void sliderValueChanged(juce::Slider* /*slider*/) override
-    {
-        if (ignoreCallbacks_ || juce::ModifierKeys::currentModifiers.isRightButtonDown()) { return; }
-        attachment_.setValueAsPartOfGesture(static_cast<float>(slider_.getValue()));
-    }
+    void setValue(float newValue);
 
-    void sliderDragStarted(juce::Slider* /*slider*/) override { attachment_.beginGesture(); }
-    void sliderDragEnded(juce::Slider* /*slider*/) override { attachment_.endGesture(); }
+    void sliderValueChanged(juce::Slider* /*slider*/) override;
+    void sliderDragStarted(juce::Slider* /*slider*/) override;
+    void sliderDragEnded(juce::Slider* /*slider*/) override;
 
     juce::Slider& slider_;
     ValueTreeAttachment<float> attachment_;
