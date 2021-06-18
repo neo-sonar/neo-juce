1d816efb171900407f3d498f892b434a24f26f06
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-06-19 01:28:48 +0200

    Fix clang-tidy warnings

--- modules/mc_dsp/airwindows/mc_AirWindowsCoils2.hpp
+++ modules/mc_dsp/airwindows/mc_AirWindowsCoils2.hpp
@@ -30,25 +30,25 @@ struct AirWindowsCoils2
         overallscale *= spec_.sampleRate;
 
         double distScaling = pow(1.0 - parameter_.saturation, 2);
-        if (distScaling < 0.0001) distScaling = 0.0001;
-        biquadA[0]            = 600.0 / spec_.sampleRate;
-        biquadA[1]            = 0.01 + (pow(parameter_.cheapness, 2) * 0.5);
-        long double iirAmount = biquadA[1] / overallscale;
-        double K              = tan(M_PI * biquadA[0]);
-        double norm           = 1.0 / (1.0 + K / biquadA[1] + K * K);
-        biquadA[2]            = K / biquadA[1] * norm;
-        biquadA[4]            = -biquadA[2];
-        biquadA[5]            = 2.0 * (K * K - 1.0) * norm;
-        biquadA[6]            = (1.0 - K / biquadA[1] + K * K) * norm;
-        biquadB[0]            = (21890.0 - (biquadA[1] * 890.0)) / spec_.sampleRate;
-        biquadB[1]            = 0.89;
-        K                     = tan(M_PI * biquadB[0]);
-        norm                  = 1.0 / (1.0 + K / biquadB[1] + K * K);
-        biquadB[2]            = K * K * norm;
-        biquadB[3]            = 2.0 * biquadB[2];
-        biquadB[4]            = biquadB[2];
-        biquadB[5]            = 2.0 * (K * K - 1.0) * norm;
-        biquadB[6]            = (1.0 - K / biquadB[1] + K * K) * norm;
+        if (distScaling < 0.0001) { distScaling = 0.0001; }
+        biquadA_[0]           = 600.0 / spec_.sampleRate;
+        biquadA_[1]           = 0.01 + (pow(parameter_.cheapness, 2) * 0.5);
+        long double iirAmount = biquadA_[1] / overallscale;
+        double k              = tan(M_PI * biquadA_[0]);
+        double norm           = 1.0 / (1.0 + k / biquadA_[1] + k * k);
+        biquadA_[2]           = k / biquadA_[1] * norm;
+        biquadA_[4]           = -biquadA_[2];
+        biquadA_[5]           = 2.0 * (k * k - 1.0) * norm;
+        biquadA_[6]           = (1.0 - k / biquadA_[1] + k * k) * norm;
+        biquadB_[0]           = (21890.0 - (biquadA_[1] * 890.0)) / spec_.sampleRate;
+        biquadB_[1]           = 0.89;
+        k                     = tan(M_PI * biquadB_[0]);
+        norm                  = 1.0 / (1.0 + k / biquadB_[1] + k * k);
+        biquadB_[2]           = k * k * norm;
+        biquadB_[3]           = 2.0 * biquadB_[2];
+        biquadB_[4]           = biquadB_[2];
+        biquadB_[5]           = 2.0 * (k * k - 1.0) * norm;
+        biquadB_[6]           = (1.0 - k / biquadB_[1] + k * k) * norm;
         double wet            = parameter_.dryWet;
 
         auto sampleFrames = buffer.getNumSamples();
@@ -56,64 +56,70 @@ struct AirWindowsCoils2
         {
             long double inputSampleL = *in1;
             long double inputSampleR = *in2;
-            if (fabs(inputSampleL) < 1.18e-37) inputSampleL = fpdL * 1.18e-37;
-            if (fabs(inputSampleR) < 1.18e-37) inputSampleR = fpdR * 1.18e-37;
+            if (std::abs(inputSampleL) < 1.18e-37) { inputSampleL = fpdL_ * 1.18e-37; }
+            if (std::abs(inputSampleR) < 1.18e-37) { inputSampleR = fpdR_ * 1.18e-37; }
             long double drySampleL = inputSampleL;
             long double drySampleR = inputSampleR;
 
-            if (biquadA[0] < 0.49999)
+            if (biquadA_[0] < 0.49999)
             {
-                long double tempSample = (inputSampleL * biquadA[2]) + biquadA[7];
-                biquadA[7]             = -(tempSample * biquadA[5]) + biquadA[8];
-                biquadA[8]             = (inputSampleL * biquadA[4]) - (tempSample * biquadA[6]);
+                long double tempSample = (inputSampleL * biquadA_[2]) + biquadA_[7];
+                biquadA_[7]            = -(tempSample * biquadA_[5]) + biquadA_[8];
+                biquadA_[8]            = (inputSampleL * biquadA_[4]) - (tempSample * biquadA_[6]);
                 inputSampleL           = tempSample;
-                tempSample             = (inputSampleR * biquadA[2]) + biquadA[9];
-                biquadA[9]             = -(tempSample * biquadA[5]) + biquadA[10];
-                biquadA[10]            = (inputSampleR * biquadA[4]) - (tempSample * biquadA[6]);
+                tempSample             = (inputSampleR * biquadA_[2]) + biquadA_[9];
+                biquadA_[9]            = -(tempSample * biquadA_[5]) + biquadA_[10];
+                biquadA_[10]           = (inputSampleR * biquadA_[4]) - (tempSample * biquadA_[6]);
                 inputSampleR           = tempSample;  // create bandpass of clean tone
             }
             long double diffSampleL = (drySampleL - inputSampleL) / distScaling;
             long double diffSampleR = (drySampleR - inputSampleR) / distScaling;  // mids notched out
-            if (biquadB[0] < 0.49999)
+            if (biquadB_[0] < 0.49999)
             {
-                long double tempSample = (diffSampleL * biquadB[2]) + biquadB[7];
-                biquadB[7]             = (diffSampleL * biquadB[3]) - (tempSample * biquadB[5]) + biquadB[8];
-                biquadB[8]             = (diffSampleL * biquadB[4]) - (tempSample * biquadB[6]);
+                long double tempSample = (diffSampleL * biquadB_[2]) + biquadB_[7];
+                biquadB_[7]            = (diffSampleL * biquadB_[3]) - (tempSample * biquadB_[5]) + biquadB_[8];
+                biquadB_[8]            = (diffSampleL * biquadB_[4]) - (tempSample * biquadB_[6]);
                 diffSampleL            = tempSample;
-                tempSample             = (diffSampleR * biquadB[2]) + biquadB[9];
-                biquadB[9]             = (diffSampleR * biquadB[3]) - (tempSample * biquadB[5]) + biquadB[10];
-                biquadB[10]            = (diffSampleR * biquadB[4]) - (tempSample * biquadB[6]);
+                tempSample             = (diffSampleR * biquadB_[2]) + biquadB_[9];
+                biquadB_[9]            = (diffSampleR * biquadB_[3]) - (tempSample * biquadB_[5]) + biquadB_[10];
+                biquadB_[10]           = (diffSampleR * biquadB_[4]) - (tempSample * biquadB_[6]);
                 diffSampleR            = tempSample;
             }
-            hysteresisL = (hysteresisL * (1.0 - iirAmount)) + (diffSampleL * iirAmount);
-            if (fabs(hysteresisL) < 1.18e-37)
-                hysteresisL = 0.0;
+            hysteresisL_ = (hysteresisL_ * (1.0 - iirAmount)) + (diffSampleL * iirAmount);
+            if (std::abs(hysteresisL_) < 1.18e-37) { hysteresisL_ = 0.0; }
             else
-                diffSampleL -= hysteresisL;
-            if (diffSampleL > 1.571)
-                diffSampleL = 1.571;
+            {
+                diffSampleL -= hysteresisL_;
+            }
+            if (diffSampleL > 1.571) { diffSampleL = 1.571; }
             else if (diffSampleL < -1.571)
+            {
                 diffSampleL = -1.571;
-            if (hysteresisL > 1.571)
-                hysteresisL = 1.571;
-            else if (hysteresisL < -1.571)
-                hysteresisL = -1.571;
+            }
+            if (hysteresisL_ > 1.571) { hysteresisL_ = 1.571; }
+            else if (hysteresisL_ < -1.571)
+            {
+                hysteresisL_ = -1.571;
+            }
 
-            hysteresisR = (hysteresisR * (1.0 - iirAmount)) + (diffSampleR * iirAmount);
-            if (fabs(hysteresisR) < 1.18e-37)
-                hysteresisR = 0.0;
+            hysteresisR_ = (hysteresisR_ * (1.0 - iirAmount)) + (diffSampleR * iirAmount);
+            if (std::abs(hysteresisR_) < 1.18e-37) { hysteresisR_ = 0.0; }
             else
-                diffSampleR -= hysteresisR;
-            if (diffSampleR > 1.571)
-                diffSampleR = 1.571;
+            {
+                diffSampleR -= hysteresisR_;
+            }
+            if (diffSampleR > 1.571) { diffSampleR = 1.571; }
             else if (diffSampleR < -1.571)
+            {
                 diffSampleR = -1.571;
-            if (hysteresisR > 1.571)
-                hysteresisR = 1.571;
-            else if (hysteresisR < -1.571)
-                hysteresisR = -1.571;
-            inputSampleL += (sin(diffSampleL) - sin(hysteresisL)) * distScaling;
-            inputSampleR += (sin(diffSampleR) - sin(hysteresisR)) * distScaling;  // apply transformer distortions
+            }
+            if (hysteresisR_ > 1.571) { hysteresisR_ = 1.571; }
+            else if (hysteresisR_ < -1.571)
+            {
+                hysteresisR_ = -1.571;
+            }
+            inputSampleL += (sin(diffSampleL) - sin(hysteresisL_)) * distScaling;
+            inputSampleR += (sin(diffSampleR) - sin(hysteresisR_)) * distScaling;  // apply transformer distortions
 
             if (wet != 1.0)
             {
@@ -124,15 +130,15 @@ struct AirWindowsCoils2
             // begin 32 bit stereo floating point dither
             int expon;
             frexpf((float)inputSampleL, &expon);
-            fpdL ^= fpdL << 13;
-            fpdL ^= fpdL >> 17;
-            fpdL ^= fpdL << 5;
-            inputSampleL += ((double(fpdL) - uint32_t(0x7fffffff)) * 5.5e-36l * pow(2, expon + 62));
+            fpdL_ ^= fpdL_ << 13;
+            fpdL_ ^= fpdL_ >> 17;
+            fpdL_ ^= fpdL_ << 5;
+            inputSampleL += ((double(fpdL_) - uint32_t(0x7fffffff)) * 5.5e-36L * pow(2, expon + 62));
             frexpf((float)inputSampleR, &expon);
-            fpdR ^= fpdR << 13;
-            fpdR ^= fpdR >> 17;
-            fpdR ^= fpdR << 5;
-            inputSampleR += ((double(fpdR) - uint32_t(0x7fffffff)) * 5.5e-36l * pow(2, expon + 62));
+            fpdR_ ^= fpdR_ << 13;
+            fpdR_ ^= fpdR_ >> 17;
+            fpdR_ ^= fpdR_ << 5;
+            inputSampleR += ((double(fpdR_) - uint32_t(0x7fffffff)) * 5.5e-36L * pow(2, expon + 62));
             // end 32 bit stereo floating point dither
 
             *out1 = inputSampleL;
@@ -150,26 +156,26 @@ struct AirWindowsCoils2
 
         for (int x = 0; x < 15; x++)
         {
-            biquadA[x] = 0.0;
-            biquadB[x] = 0.0;
+            biquadA_[x] = 0.0;
+            biquadB_[x] = 0.0;
         }
-        hysteresisL = hysteresisR = 0.0;
-        fpdL                      = 1.0;
-        while (fpdL < 16386) fpdL = rand() * UINT32_MAX;
-        fpdR = 1.0;
-        while (fpdR < 16386) fpdR = rand() * UINT32_MAX;
+        hysteresisL_ = hysteresisR_ = 0.0;
+        fpdL_                       = 1.0;
+        while (fpdL_ < 16386) { fpdL_ = rand() * UINT32_MAX; }
+        fpdR_ = 1.0;
+        while (fpdR_ < 16386) { fpdR_ = rand() * UINT32_MAX; }
     }
 
 private:
     juce::dsp::ProcessSpec spec_ {};
     Parameter parameter_ {};
 
-    long double biquadA[15];
-    long double biquadB[15];
-    long double hysteresisL;
-    long double hysteresisR;
-    uint32_t fpdL;
-    uint32_t fpdR;
+    long double biquadA_[15] {};
+    long double biquadB_[15] {};
+    long double hysteresisL_ {};
+    long double hysteresisR_ {};
+    uint32_t fpdL_ {};
+    uint32_t fpdR_ {};
     // default stuff
 
     //    float A;
