3c17b8e1881daeaa3caa1233fccf4a9a5f338726
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-07-15 01:19:24 +0200

    Remove modulation module

--- modules/mc_modulation/core/mc_ModulationMatrix_Test.cpp
+++
@@ -1,6 +0,0 @@
-#include <mc_modulation/mc_modulation.hpp>
-
-#include "catch2/catch_approx.hpp"
-#include "catch2/catch_test_macros.hpp"
-
-TEST_CASE("modulation/core: ModulationMatrix", "[modulation][core]") { SUCCEED(); }
--- modules/mc_modulation/mc_modulation.cpp
+++
@@ -1,7 +0,0 @@
-#include "mc_modulation/mc_modulation.hpp"
-
-#include "core/mc_ModulationMatrix.cpp"  // NOLINT(bugprone-suspicious-include)
-#include "core/mc_Modulator.cpp"         // NOLINT(bugprone-suspicious-include)
-#include "core/mc_ModulatorList.cpp"     // NOLINT(bugprone-suspicious-include)
-
-#include "modulator/mc_ModulatorMacro.cpp"  // NOLINT(bugprone-suspicious-include)
