77dee7c3a2b123d73c60bf44c6555db37e2ce411
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-10-01 08:49:02 +0200

    Update JUCE & fix deprecated warnings

--- modules/mc_core/javascript/mc_JSONUtils.cpp
+++ modules/mc_core/javascript/mc_JSONUtils.cpp
@@ -2,7 +2,8 @@
 namespace mc {
 auto JSONUtils::loadFromURL(juce::URL const& url) -> juce::var
 {
-    auto response = url.createInputStream(false);
+    auto options  = juce::URL::InputStreamOptions { juce::URL::ParameterHandling::inAddress };
+    auto response = url.createInputStream(options);
     jassert(response != nullptr);
     return juce::JSON::parse(*response);
 }
--- modules/mc_graphics/images/mc_ImageUtils.cpp
+++ modules/mc_graphics/images/mc_ImageUtils.cpp
@@ -1,7 +1,8 @@
 namespace mc {
 auto ImageUtils::loadFromURL(juce::URL const& url) -> juce::Image
 {
-    auto response = url.createInputStream(false);
+    auto options  = juce::URL::InputStreamOptions { juce::URL::ParameterHandling::inAddress };
+    auto response = url.createInputStream(options);
     auto block    = juce::MemoryBlock {};
     response->readIntoMemoryBlock(block);
     auto mis = juce::MemoryInputStream(block, false);
