afd34ab8a7818e429af4257c2da898dc9338b3e1
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-10-03 02:01:24 +0200

    Move thread safe queues to mcSTL

--- modules/mc_core/container/mc_Variant_Test.cpp
+++
@@ -1,9 +0,0 @@
-#include "mc_core/mc_core.hpp"
-
-#include <catch2/catch.hpp>
-
-TEST_CASE("core/container: variant", "[core][container]")
-{
-    auto v = mc::variant<int, float> { 143.0f };
-    CHECK(mc::get<float>(v) == 143.0f);
-}
