4211ad7d0dfc042f70a8e1428093c95fe7db9404
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-25 20:00:49 +0200

    [dsp] Use Vector in RingBuffer

--- modules/mc_dsp/container/RingBuffer.hpp
+++ modules/mc_dsp/container/RingBuffer.hpp
@@ -5,7 +5,7 @@ namespace mc {
 template <typename T>
 struct RingBuffer {
     using value_type = T;
-    using size_type  = std::uint32_t;
+    using size_type  = std::size_t;
 
     RingBuffer();
     explicit RingBuffer(size_type size);
@@ -20,8 +20,7 @@ struct RingBuffer {
     [[nodiscard]] auto operator[](size_type index) const noexcept -> float;
 
 private:
-    UniquePtr<T[]> _buffer { nullptr }; // NOLINT(modernize-avoid-c-arrays)
-    size_type _size { 0 };
+    Vector<T> _buffer {};
     size_type _writePosition { 0 };
 };
 
@@ -40,40 +39,39 @@ template <typename T>
 auto RingBuffer<T>::operator[](size_type index) const noexcept -> float
 {
     auto const i = _writePosition + index;
-    if (i < _size) { return _buffer[i]; }
-    return _buffer[i - _size];
+    if (i < _buffer.size()) { return _buffer[i]; }
+    return _buffer[i - _buffer.size()];
 }
 
 template <typename T>
 auto RingBuffer<T>::clear() -> void
 {
-    ranges::fill(_buffer.get(), std::next(_buffer.get(), _size), T {});
+    ranges::fill(_buffer, T {});
 }
 
 template <typename T>
 auto RingBuffer<T>::resize(size_type size) -> void
 {
-    _size   = size;
-    _buffer = makeUnique<T[]>(size); // NOLINT(modernize-avoid-c-arrays)
+    _buffer = Vector<T>(size);
     clear();
 }
 
 template <typename T>
 auto RingBuffer<T>::size() const noexcept -> size_type
 {
-    return _size;
+    return _buffer.size();
 }
 
 template <typename T>
 auto RingBuffer<T>::empty() const noexcept -> bool
 {
-    return size() == 0;
+    return size() == 0U;
 }
 
 template <typename T>
 auto RingBuffer<T>::push(T value) -> void
 {
     _buffer[_writePosition++] = value;
-    if (_writePosition == _size) { _writePosition = 0; }
+    if (_writePosition == _buffer.size()) { _writePosition = 0; }
 }
 } // namespace mc
