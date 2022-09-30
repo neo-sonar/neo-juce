4b305bafda6dbd941496c87b2d1539aa10f0acc8
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-30 16:38:13 +0200

    [audio_basics] Fix warning in tests

--- modules/mcj_audio_basics/buffers/buffer_with_samplerate.test.cpp
+++ modules/mcj_audio_basics/buffers/buffer_with_samplerate.test.cpp
@@ -6,7 +6,7 @@
 TEMPLATE_TEST_CASE("audio_basics/buffers: toVector(BufferWithSampleRate)", "[audio_basics]", float, double)
 {
     auto numSamples = GENERATE(32, 143, 256, 512, 1111, 4096);
-    auto buffer     = mc::BufferWithSampleRate<TestType> { juce::AudioBuffer<TestType> { 1, numSamples } };
+    auto buffer     = mc::BufferWithSampleRate<TestType> { juce::AudioBuffer<TestType> { 1, numSamples }, 44'100.0 };
     auto vec        = mc::toVector(buffer);
     REQUIRE(vec.size() == static_cast<std::size_t>(numSamples));
 }
