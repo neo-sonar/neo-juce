70914505ce3ee033c433f5d3bf232007287d003c
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-30 14:09:42 +0200

    [dsp] Add tests to channel(juce::dsp::AudioBlock)

---
+++ modules/mcj_dsp/container/audio_block.test.cpp
@@ -0,0 +1,14 @@
+#include <mcj_dsp/mcj_dsp.hpp>
+
+#include <catch2/catch_template_test_macros.hpp>
+
+TEMPLATE_TEST_CASE("dsp/container: channel(juce::dsp::AudioBlock)", "[dsp]", float, double)
+{
+    auto memory = juce::HeapBlock<char> {};
+    auto block  = juce::dsp::AudioBlock<TestType> { memory, 2UL, 512UL };
+    REQUIRE(mc::channel(block, 0).size() == 512UL);
+    REQUIRE(mc::channel(block, 1).size() == 512UL);
+
+    REQUIRE(mc::channel(block, 0).data() == block.getChannelPointer(0));
+    REQUIRE(mc::channel(block, 1).data() == block.getChannelPointer(1));
+}
