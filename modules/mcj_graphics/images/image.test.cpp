f41b3d3c9214317798ab7d4e18101160db686085
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-30 13:17:19 +0200

    [graphics] Add tests to VariantConverter<juce::Image>

--- modules/mcj_graphics/images/image.test.cpp
+++ modules/mcj_graphics/images/image.test.cpp
@@ -13,4 +13,18 @@ TEST_CASE("graphics/images: VariantConverter<juce::Image>", "[graphics]")
         REQUIRE(original.getBounds() == result.getBounds());
         REQUIRE(original.getFormat() == result.getFormat());
     }
+
+    SECTION("red_256x256.jpg")
+    {
+        auto const path     = juce::File::getCurrentWorkingDirectory().getChildFile("test_data/img/red_256x256.jpg");
+        auto const original = juce::ImageCache::getFromFile(path);
+        REQUIRE(original.getBounds().getWidth() == 256);
+        REQUIRE(original.getBounds().getHeight() == 256);
+
+        auto const value  = juce::VariantConverter<juce::Image>::toVar(original);
+        auto const result = juce::VariantConverter<juce::Image>::fromVar(value);
+        REQUIRE(original.isValid() == result.isValid());
+        REQUIRE(original.getBounds() == result.getBounds());
+        REQUIRE(original.getFormat() == result.getFormat());
+    }
 }
