0dc2b99c84a238961f73926903beee0a7972ce55
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-11-08 21:54:29 +0100

    [lottie] Add static color parser

--- modules/mc_lottie/property/lottie_color.hpp
+++ modules/mc_lottie/property/lottie_color.hpp
@@ -2,20 +2,25 @@
 
 namespace mc {
 struct LottieColor {
+    inline static constexpr auto const* token = "c";
+
+    [[nodiscard]] static auto parse(juce::var const& obj) -> LottieColor;
+
     double red { 0.0 };
     double green { 0.0 };
     double blue { 0.0 };
-
-    [[nodiscard]] static auto parse(juce::var const& obj) -> LottieColor
-    {
-        auto const* array = obj.getArray();
-        if (array == nullptr) { raise<InvalidArgument>("missing array of color values"); }
-        if (array->size() != 3) { raise<InvalidArgument>("wrong size for array of color values"); }
-        return {
-            static_cast<double>((*array)[0]),
-            static_cast<double>((*array)[1]),
-            static_cast<double>((*array)[2]),
-        };
-    }
 };
+
+inline auto LottieColor::parse(juce::var const& obj) -> LottieColor
+{
+    auto const* array = obj.getArray();
+    if (array == nullptr) { raise<InvalidArgument>("missing array of color values"); }
+    if (array->size() != 3) { raise<InvalidArgument>("wrong size for array of color values"); }
+    return {
+        static_cast<double>((*array)[0]),
+        static_cast<double>((*array)[1]),
+        static_cast<double>((*array)[2]),
+    };
+}
+
 } // namespace mc
---
+++ modules/mc_lottie/property/lottie_color.test.cpp
@@ -0,0 +1,39 @@
+#include <mc_lottie/mc_lottie.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+
+TEST_CASE("lottie/model: LottieColor::parse", "[lottie]")
+{
+    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[0,0,0]")).red == 0.0);
+    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[0,0,0]")).green == 0.0);
+    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[0,0,0]")).blue == 0.0);
+
+    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[1,1,1]")).red == 1.0);
+    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[1,1,1]")).green == 1.0);
+    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[1,1,1]")).blue == 1.0);
+
+    auto const* src = R"(
+        {
+            "c": {
+                "a": 0,
+                "k": [
+                    1,
+                    0.98,
+                    0.28
+                ]
+            }
+        }
+    )";
+
+    auto const obj   = juce::JSON::parse(src);
+    auto const color = mc::LottieColor::parse(obj["c"]["k"]);
+    REQUIRE(color.red == 1.0);
+    REQUIRE(color.green == Catch::Approx(0.98));
+    REQUIRE(color.blue == Catch::Approx(0.28));
+
+    auto const property = mc::StaticLottieColor::parse(obj["c"]);
+    REQUIRE(property.value.red == 1.0);
+    REQUIRE(property.value.green == Catch::Approx(0.98));
+    REQUIRE(property.value.blue == Catch::Approx(0.28));
+}
