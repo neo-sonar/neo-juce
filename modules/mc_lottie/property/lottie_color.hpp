5148e5f68b24c4d25a8f506586546607779744dc
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-11-08 22:53:46 +0100

    [lottie] Add alpha component to LottieColor

--- modules/mc_lottie/property/lottie_color.hpp
+++ modules/mc_lottie/property/lottie_color.hpp
@@ -9,18 +9,32 @@ struct LottieColor {
     double red { 0.0 };
     double green { 0.0 };
     double blue { 0.0 };
+    double alpha { 1.0 };
 };
 
 inline auto LottieColor::parse(juce::var const& obj) -> LottieColor
 {
     auto const* array = obj.getArray();
     if (array == nullptr) { raise<InvalidArgument>("missing array of color values"); }
-    if (array->size() != 3) { raise<InvalidArgument>("wrong size for array of color values"); }
-    return {
-        static_cast<double>((*array)[0]),
-        static_cast<double>((*array)[1]),
-        static_cast<double>((*array)[2]),
-    };
+
+    if (array->size() == 3) {
+        return {
+            static_cast<double>((*array)[0]),
+            static_cast<double>((*array)[1]),
+            static_cast<double>((*array)[2]),
+        };
+    }
+
+    if (array->size() == 4) {
+        return {
+            static_cast<double>((*array)[0]),
+            static_cast<double>((*array)[1]),
+            static_cast<double>((*array)[2]),
+            static_cast<double>((*array)[3]),
+        };
+    }
+
+    raise<InvalidArgument>("wrong size for array of color values. 3 or 4 (rgb & rgba)");
 }
 
 } // namespace mc
--- modules/mc_lottie/property/lottie_color.test.cpp
+++ modules/mc_lottie/property/lottie_color.test.cpp
@@ -5,13 +5,26 @@
 
 TEST_CASE("lottie/model: LottieColor::parse", "[lottie]")
 {
-    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[0,0,0]")).red == 0.0);
-    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[0,0,0]")).green == 0.0);
-    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[0,0,0]")).blue == 0.0);
+    REQUIRE_THROWS(mc::LottieColor::parse(juce::JSON::parse("{}")));
+    REQUIRE_THROWS(mc::LottieColor::parse(juce::JSON::parse("[0,0]")));
 
-    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[1,1,1]")).red == 1.0);
-    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[1,1,1]")).green == 1.0);
-    REQUIRE(mc::LottieColor::parse(juce::JSON::parse("[1,1,1]")).blue == 1.0);
+    auto const black = juce::JSON::parse("[0,0,0]");
+    REQUIRE(mc::LottieColor::parse(black).red == Catch::Approx(0.0));
+    REQUIRE(mc::LottieColor::parse(black).green == Catch::Approx(0.0));
+    REQUIRE(mc::LottieColor::parse(black).blue == Catch::Approx(0.0));
+    REQUIRE(mc::LottieColor::parse(black).alpha == Catch::Approx(1.0));
+
+    auto const white = juce::JSON::parse("[1,1,1]");
+    REQUIRE(mc::LottieColor::parse(white).red == Catch::Approx(1.0));
+    REQUIRE(mc::LottieColor::parse(white).green == Catch::Approx(1.0));
+    REQUIRE(mc::LottieColor::parse(white).blue == Catch::Approx(1.0));
+    REQUIRE(mc::LottieColor::parse(white).alpha == Catch::Approx(1.0));
+
+    auto const withAlpha = juce::JSON::parse("[0.0,0.25,0.5,0.75]");
+    REQUIRE(mc::LottieColor::parse(withAlpha).red == Catch::Approx(0.0));
+    REQUIRE(mc::LottieColor::parse(withAlpha).green == Catch::Approx(0.25));
+    REQUIRE(mc::LottieColor::parse(withAlpha).blue == Catch::Approx(0.5));
+    REQUIRE(mc::LottieColor::parse(withAlpha).alpha == Catch::Approx(0.75));
 
     auto const* src = R"(
         {
@@ -28,12 +41,14 @@ TEST_CASE("lottie/model: LottieColor::parse", "[lottie]")
 
     auto const obj   = juce::JSON::parse(src);
     auto const color = mc::LottieColor::parse(obj["c"]["k"]);
-    REQUIRE(color.red == 1.0);
+    REQUIRE(color.red == Catch::Approx(1.0));
     REQUIRE(color.green == Catch::Approx(0.98));
     REQUIRE(color.blue == Catch::Approx(0.28));
+    REQUIRE(color.alpha == Catch::Approx(1.0));
 
     auto const property = mc::StaticLottieColor::parse(obj["c"]);
-    REQUIRE(property.value.red == 1.0);
+    REQUIRE(property.value.red == Catch::Approx(1.0));
     REQUIRE(property.value.green == Catch::Approx(0.98));
     REQUIRE(property.value.blue == Catch::Approx(0.28));
+    REQUIRE(property.value.alpha == Catch::Approx(1.0));
 }
