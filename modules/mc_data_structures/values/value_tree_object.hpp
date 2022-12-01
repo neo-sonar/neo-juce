6a11da41d393d2052b79c6a72ca27b4501090b26
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-12-01 14:33:15 +0100

    [data_structures] Add UndoManager to ValueTreeObject

--- modules/mc_data_structures/values/value_tree_object.hpp
+++ modules/mc_data_structures/values/value_tree_object.hpp
@@ -3,16 +3,22 @@
 namespace mc {
 
 struct ValueTreeObject {
-    explicit ValueTreeObject(juce::ValueTree tree);
+    explicit ValueTreeObject(juce::ValueTree tree, juce::UndoManager* um = nullptr);
 
     [[nodiscard]] auto valueTree() -> juce::ValueTree& { return _valueTree; }
 
     [[nodiscard]] auto valueTree() const -> juce::ValueTree const& { return _valueTree; }
 
+    [[nodiscard]] auto undoManager() const -> juce::UndoManager* { return _undoManager; }
+
 private:
     juce::ValueTree _valueTree;
+    juce::UndoManager* _undoManager;
 };
 
-inline ValueTreeObject::ValueTreeObject(juce::ValueTree valueTree) : _valueTree { std::move(valueTree) } { }
+inline ValueTreeObject::ValueTreeObject(juce::ValueTree valueTree, juce::UndoManager* um)
+    : _valueTree { std::move(valueTree) }, _undoManager { um }
+{
+}
 
 } // namespace mc
