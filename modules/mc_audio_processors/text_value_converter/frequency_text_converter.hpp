cecc1263caf0ea54bbc30e1682fe6138cae3dd07
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-05-04 01:28:20 +0200

    [audio_processors] Fix decimal places in FrequencyTextConverter

--- modules/mc_audio_processors/text_value_converter/frequency_text_converter.hpp
+++ modules/mc_audio_processors/text_value_converter/frequency_text_converter.hpp
@@ -7,7 +7,7 @@ struct FrequencyTextConverter {
     [[nodiscard]] auto operator()(float value, int maxStringLength = 0) const -> juce::String
     {
         juce::ignoreUnused(maxStringLength);
-        return (value < 1000) ? juce::String(value, 0) + " Hz" : juce::String(value / 1000.0, 2) + " kHz";
+        return (value < 1000) ? juce::String(value, 2) + " Hz" : juce::String(value / 1000.0, 2) + " kHz";
     }
 
     [[nodiscard]] auto operator()(juce::String const& text) const -> float
