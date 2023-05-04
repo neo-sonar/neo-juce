d14abc8b2f2697e4376144308ae0456dca45d8eb
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-05-04 05:07:54 +0200

    [audio_processors] Fix FrequencyTextConverter tests

--- modules/mc_audio_processors/text_value_converter/frequency_text_converter.test.cpp
+++ modules/mc_audio_processors/text_value_converter/frequency_text_converter.test.cpp
@@ -8,14 +8,14 @@ TEST_CASE("audio_processors/text_value_converter: FrequencyTextConverter", "[aud
     SECTION("String -> float")
     {
         mc::FrequencyTextConverter const converter {};
-        REQUIRE(converter("10 Hz") == 10.0F);
+        REQUIRE(converter("10.00 Hz") == 10.0F);
         REQUIRE(converter("10.00 kHz") == 10000.0F);
     }
 
     SECTION("float -> String")
     {
         mc::FrequencyTextConverter const converter {};
-        REQUIRE(converter(10.0F) == "10 Hz");
+        REQUIRE(converter(10.0F) == "10.00 Hz");
         REQUIRE(converter(10000.0F) == "10.00 kHz");
     }
 }
